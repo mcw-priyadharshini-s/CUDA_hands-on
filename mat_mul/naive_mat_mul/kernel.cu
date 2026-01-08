7be3e72980f6d8c2496fdd4a11d807eb017ce947
Subramanian <subramanian.alagesan@multicorewareinc.com>
2026-01-08 13:53:18 +0530

    Define the declared functions, the kernel function and launch the kernel function

---
+++ mat_mul/naive_mat_mul/kernel.cu
@@ -0,0 +1,48 @@
+#include "kernel.h"
+
+__global__ void mat_mul(float *arr1, float *arr2, float* res, int m, int n, int k){
+    
+    // calculating the global index of the element
+    int row = blockIdx.y*blockDim.y+threadIdx.y;
+    int col = blockIdx.x*blockDim.x+threadIdx.x;
+
+    //calculating the results of a specific index
+    float c_val = 0.0f;
+    for (int i = 0; i<n; i++){
+        c_val += arr1[row*n+i]*arr2[i*k+col];
+    }
+
+    if (row<n && col<k)
+        res[row*k+col] = c_val;
+
+}
+
+void mat_mul_launch(float *arr1, float *arr2, float* res, int m, int n, int k){
+    int x_dim = 16;
+    int y_dim = 16;
+    
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+    
+    // defining the number of threads per block
+    dim3 threadsPerBlock(x_dim, y_dim, 1);
+    
+    // defining the number of blocks per grid
+    dim3 blocksPerGrid((k-1)/threadsPerBlock.x+1, (n-1)/threadsPerBlock.y+1, 1);
+
+    cudaEventRecord(start);
+    // kernel launch
+    mat_mul<<<blocksPerGrid, threadsPerBlock>>>(arr1, arr2, res, m, n, k);
+
+    cudaEventRecord(stop);
+
+    cudaEventSynchronize(stop);
+
+    float elapsedTime= 0.0f;
+    cudaEventElapsedTime(&elapsedTime,start,stop);
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
+
+    cout<<"gpu time: "<<elapsedTime<<" ms\n";
+}
